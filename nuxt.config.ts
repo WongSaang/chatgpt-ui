233eb9c27a7a01d34b1b441be1831f35a1eaae77
Rafi <rafiwx@gmail.com>
2023-02-14 18:32:49 +0800

    feat: i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,5 +22,37 @@ export default defineNuxtConfig({
         'material-design-icons-iconfont/dist/material-design-icons.css',
         'highlight.js/styles/panda-syntax-dark.css',
     ],
-    modules: ['@nuxtjs/color-mode']
+    modules: [
+        '@nuxtjs/color-mode',
+        '@nuxtjs/i18n'
+    ],
+    i18n: {
+        // add `vueI18n` option to `@nuxtjs/i18n` module options
+        vueI18n: {
+            legacy: false,
+            locale: 'en',
+            fallbackLocale: 'en',
+            messages: {
+                en: {
+                    welcome: 'Welcome'
+                },
+                fr: {
+                    welcome: 'Bienvenue'
+                }
+            }
+        },
+        locales: [
+            {
+                code: 'en',
+                file: 'en-US.js',
+            },
+            {
+                code: 'cn',
+                file: 'zn-CN.js',
+            }
+        ],
+        lazy: true,
+        langDir: 'lang',
+        defaultLocale: 'en'
+    }
 })
