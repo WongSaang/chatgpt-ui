23f1cda22938a68e50e1cea1b24e01212a6e880f
ZeekoZhu <vaezt@outlook.com>
2023-06-25 23:21:16 +0800

    feat: add gpt-3.5-turbo-16k and gpt-4-32k

--- utils/enums.js
+++ utils/enums.js
@@ -1,29 +1,46 @@
-
 export const STORAGE_KEY = {
-    MODELS: 'models',
-    CURRENT_MODEL: 'current_model',
-    OPENAI_API_KEY: 'openai_api_key',
+  MODELS: 'models',
+  CURRENT_MODEL: 'current_model',
+  OPENAI_API_KEY: 'openai_api_key',
 }
 
 export const MODELS = {
-    'gpt-3.5-turbo': {
-        name: 'gpt-3.5-turbo',
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-        total_tokens: 4096,
-        max_tokens: 1000,
-        temperature: 0.7,
-        top_p: 1.0
-    },
-    'gpt-4': {
-        name: 'gpt-4',
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-        total_tokens: 8192,
-        max_tokens: 2000,
-        temperature: 0.7,
-        top_p: 1.0
-    }
+  'gpt-3.5-turbo': {
+    name: 'gpt-3.5-turbo',
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+    total_tokens: 4096,
+    max_tokens: 1000,
+    temperature: 0.7,
+    top_p: 1.0
+  },
+  'gpt-3.5-turbo-16k': {
+    name: 'gpt-3.5-16k',
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+    total_tokens: 16384,
+    max_tokens: 4000,
+    temperature: 0.7,
+    top_p: 1.0
+  },
+  'gpt-4-32k': {
+    name: 'gpt-4-32k',
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+    total_tokens: 32768,
+    max_tokens: 8000,
+    temperature: 0.7,
+    top_p: 1.0
+  },
+  'gpt-4': {
+    name: 'gpt-4',
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+    total_tokens: 8192,
+    max_tokens: 2000,
+    temperature: 0.7,
+    top_p: 1.0
+  }
 }
 
 export const DEFAULT_MODEL_NAME = 'gpt-3.5-turbo'
